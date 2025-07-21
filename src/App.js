302faae2dc881e1927b6149b18af17c4ac28b08c
qkdwn <bjhbjh0313@gmail.com>
2025-07-21 11:55:41 +0900

    ch3-14 MongDB

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 
-
 import Test from "./Test"
 import Heading from "./Component/Heading"
 import List from "./Component/List"
 import Upload from "./Component/Upload"
 
 function App() {
-    const [ContentList, setContentList] = React.useState([]);
-  
+  const [ContentList, setContentList] = React.useState([])
+
   return (
     <>
-    <Heading />
-    <Routes>
-      <Route path="/List" element={<List ContentList = { ContentList } setContentList = { setContentList }/>} />
-      <Route path="/Upload" element={<Upload ContentList = { ContentList } setContentList = { setContentList }/>} />
-    </Routes>
+      <Heading />
+      <Routes>
+        <Route path="/List" element={<List ContentList={ContentList} setContentList={setContentList} />} />
+        <Route path="/Upload" element={<Upload ContentList={ContentList} setContentList={setContentList} />} />
+      </Routes>
     </>
   )
 }
